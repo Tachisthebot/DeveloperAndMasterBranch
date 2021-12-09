11551bdc2a1eabe312bc48d55918f453e70c625b
Casper <tachpro2@gmail.com>
2021-12-09 00:26:02 -0500

    new commit

--- src/test/java/Work/Works.java
+++ src/test/java/Work/Works.java
@@ -2,10 +2,10 @@ package Work;
 
 public class Works {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("bYE");
-=======
+
         System.out.println("work");
->>>>>>> Gulnaz
+
     }
 }
